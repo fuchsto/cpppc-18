d7cb84f9f9ae94fc27946a689b12bb88d6e04dde
Tobias Fuchs <fuchst@nm.ifi.lmu.de>
2018-06-01 08:49:54 +0200

    Updated reference implementation of sparse_array

--- assignment-04/s-04/solution/sparse_array.h
+++ assignment-04/s-04/solution/sparse_array.h
@@ -1,343 +1,351 @@
 #ifndef CPPPC__S03__SPARSE_ARRAY_H__INCLUDED
 #define CPPPC__S03__SPARSE_ARRAY_H__INCLUDED
 
-#include <unordered_map>
-#include <limits>
-#include <iterator>
+#include <map>
 
 
 namespace cpppc {
 
 namespace detail {
 
-// Forward-declaration
 template <class SparseArrayT>
 class sparse_array_proxy_ref;
 
-// Forward-declaration
 template <class SparseArrayT>
-class sparse_array_iterator;
+class sparse_array_iterator {
+  using self_t    = sparse_array_iterator<SparseArrayT>;
+  using element_t = typename SparseArrayT::value_t;
 
-
-template <class SparseArrayT>
-class sparse_array_iterator
-{
-  using self_t            = sparse_array_iterator<SparseArrayT>;
-  using sparse_array_t    = SparseArrayT;
-
-public:
+ public:
   using iterator_category = std::random_access_iterator_tag;
-  using value_type        = typename SparseArrayT::value_type;
-  using difference_type   = std::ptrdiff_t;
-  using reference         = sparse_array_proxy_ref<SparseArrayT>;
-                            // what about const-references?
-                            // Well: SparseArrayT would be a
-                            // const SparseArrayT then.
-                            // With reference type:
-                            //   SparseArrayT::reference
-                            // =>
-                            //   sparse_array_proxy_ref<const sparse_array<int, N>>
-                            // So all should be well.
+  using difference_type   = typename SparseArrayT::difference_type;
+  using value_type        = element_t;
+  using pointer           = element_t *;
+  using reference         = element_t &;
+  using proxy_reference_t = sparse_array_proxy_ref<SparseArrayT>;
 
-public:
-  // Iterators must be default-constructible
-  sparse_array_iterator()                     = default;
-  ~sparse_array_iterator()                    = default;
+ public:
+  sparse_array_iterator() = delete;
+  sparse_array_iterator(SparseArrayT & sarr, difference_type pos)
+      : _sarr(&sarr), _pos(pos) {}
 
-  sparse_array_iterator(const self_t & other) = default;
-  sparse_array_iterator(self_t && other)      = default;
-
-  sparse_array_iterator(sparse_array_t & sa, int pos)
-  : _sa(&sa), _pos(pos)
-  { }
-
-  self_t & operator=(const self_t & other)      = default;
-  self_t & operator=(self_t && other)           = default;
-
-  bool operator==(const self_t & rhs) const {
-    return this == &rhs || (_sa == rhs._sa && _pos == rhs._pos);
+  inline proxy_reference_t operator*() {
+    return proxy_reference_t(*_sarr, _pos);
   }
 
-  bool operator!=(const self_t & rhs) const {
-    return not (*this == rhs);
+  inline const element_t &operator*() const {
+    return _sarr->_cgetValueOrDefault(_pos);
   }
 
-  bool operator<(const self_t & rhs) const {
-    return _pos < rhs._pos;
-  }
-  bool operator<=(const self_t & rhs) const {
-    return !(*this > rhs);
-  }
-
-  bool operator>(const self_t & rhs) const {
-    return _pos > rhs._pos;
-  }
-  bool operator>=(const self_t & rhs) const {
-    return !(*this < rhs);
-  }
-
-  reference operator*() {
-    return reference(*_sa, _pos);
-  }
-
-  self_t operator++(int) {
-    auto old = *this;
-    ++_pos;
-    return old;
-  }
-
-  self_t & operator++() {
+  inline self_t &operator++() {
     ++_pos;
     return *this;
   }
 
-  self_t operator--(int) {
-    auto old = *this;
-    --_pos;
-    return old;
-  }
-
-  self_t & operator--() {
+  inline self_t &operator--() {
     --_pos;
     return *this;
   }
 
-  self_t operator+(int offset) {
-    auto it = *this;
-    it += offset;
-    return it;
+  inline self_t operator++(int) {
+    self_t copy(*this);
+    ++_pos;
+    return copy;
+  }
+  inline self_t operator--(int) {
+    self_t copy(*this);
+    --_pos;
+    return copy;
+  }
+  inline self_t operator+(const int &n) {
+    self_t copy(*this);
+    copy._pos += n;
+    return copy;
+  }
+  inline self_t operator-(const int &n) {
+    self_t copy(*this);
+    copy._pos -= n;
+    return copy;
+  }
+  inline difference_type operator-(const self_t &rhs) const {
+    return _pos - rhs._pos;
   }
 
-  self_t operator-(int offset) {
-    auto it = *this;
-    it -= offset;
-    return it;
+  inline self_t &operator+=(const difference_type n) {
+    _pos += n;
+    return *this;
   }
-
-  self_t & operator+=(int offset) {
-    _pos += offset;
+  inline self_t &operator-=(const difference_type n) {
+    _pos -= n;
+    return *this;
+  }
+  inline self_t &operator=(const self_t &other) {
+    _pos = other._pos;
     return *this;
   }
 
-  self_t & operator-=(int offset) {
-    _pos -= offset;
-    return *this;
+  inline self_t operator[](int offset) const {
+    return *(*this + offset);
   }
 
-  reference operator[](int offset) const {
-    _pos += offset;
-    return *(*this);
+  // Comparison
+  inline bool operator==(const self_t &other) const {
+    return (_sarr == other._sarr && _pos == other._pos);
+  }
+  inline bool operator!=(const self_t &other) const {
+    return !(_sarr == other._sarr && _pos == other._pos);
+  }
+  inline bool operator<=(const self_t &other) const {
+    return _pos <= other._pos;
+  }
+  inline bool operator<(const self_t &other) const {
+    return _pos < other._pos;
+  }
+  inline bool operator>=(const self_t &other) const {
+    return _pos >= other._pos;
+  }
+  inline bool operator>(const self_t &other) const {
+    return _pos > other._pos;
   }
 
-private:
-  // ... and default-construction means we can't use a reference member.
-  // And actually reference members are (usually) not what you want
-  // anyways.
-  sparse_array_t * _sa  = nullptr;
-  int              _pos = 0;
+ private:
+  SparseArrayT * _sarr = nullptr;
+  size_t         _pos  = 0;
 };
 
 template <class SparseArrayT>
-class sparse_array_proxy_ref
-{
-  using self_t         = sparse_array_proxy_ref<SparseArrayT>;
-  using sparse_array_t = SparseArrayT;
-  using value_type     = typename SparseArrayT::value_type;
+class sparse_array_proxy_ref {
+  typedef sparse_array_proxy_ref<SparseArrayT> self_t;
+  using element_t = typename SparseArrayT::value_t;
+  using index_t   = typename SparseArrayT::difference_type;
 
-  public:
-    // This type should mimic a reference, like T&.
-    // So it cannot be constructed without an element if refers to.
-    sparse_array_proxy_ref() = delete;
+ private:
+  SparseArrayT & _sarr;
+  index_t        _pos;
 
-    sparse_array_proxy_ref(sparse_array_t & sa, int index)
-    : _sa(sa), _index(index)
-    { }
-
-    //
-    //  auto & value = (sa[1] = sa[2]) = sa[3];
-    //
-    //
-    self_t & operator=(const value_type & rhs) {
-      _sa._set_value(_index, rhs);
+ public:
+  sparse_array_proxy_ref() = delete;
+  sparse_array_proxy_ref(SparseArrayT &sarr, const index_t &pos)
+    : _sarr(sarr), _pos(pos) {}
+  sparse_array_proxy_ref(self_t && other) = default;
+  self_t & operator=(self_t && other) {
+    if (this == &other) {
       return *this;
     }
+    _sarr[_pos] = static_cast<element_t>(other._sarr[other._pos]);
+    _pos        = other._pos;
+    return *this;
+  }
+  sparse_array_proxy_ref(const self_t &other) = delete;
+  self_t &operator=(const self_t &other) = delete;
 
-    operator value_type() const {
-      return _sa[_index];  // *this : const sparse_array_proxy_ref<...>
-                           // _sa   : const sparse_array_t
-    
+  self_t &operator=(const element_t &t) {
+    if (t != _sarr._default_value) {
+      _sarr._values[_pos] = t;
+    } else {
+      _sarr._values.erase(_pos);
     }
+    return *this;
+  }
 
-    //  int value_a = 12;
-    //  int value_b = 12;
-    //
-    //  int & ref_a = value_a;
-    //  int & ref_b = value_b;
-    //
-    //  ref_a == ref_b
-    //
-    bool operator==(const self_t & rhs) const {
-      return (this == &rhs) ||
-             static_cast<value_type>(*this) == static_cast<value_type>(rhs);
+  operator element_t() const {
+    element_t ret = _sarr._cgetValueOrDefault(_pos);
+    return ret;
+  }
+
+  bool operator==(const self_t &other) const {
+    if (this == &other) {
+      return true;
     }
+    if (_pos != other._pos) {
+      return false;
+    }
+    return static_cast<element_t>(*this) == static_cast<element_t>(other);
+  }
+  bool operator!=(const self_t &other) const { return !(*this == other); }
 
-  private:
-    sparse_array_t & _sa;
-    int              _index;
+  template <class T1, class T2>
+  friend inline void swap(sparse_array_proxy_ref<T1> &&first,
+                          sparse_array_proxy_ref<T2> &&second);
 };
 
-} // namespace detail
+template <class T1, class T2>
+inline void swap(sparse_array_proxy_ref<T1> &&first,
+                 sparse_array_proxy_ref<T2> &&second) {
+  typedef typename T1::value_t value_t;
+  value_t                      vfirst  = first;
+  value_t                      vsecond = second;
+  if (vfirst == vsecond) {
+    return;
+  }
 
+  const auto pfirst  = first._pos;
+  const auto psecond = second._pos;
+  first._sarr[pfirst]   = vsecond;
+  second._sarr[psecond] = vfirst;
+  return;
+}
+
+}  // namespace detail
 
 template <class T, std::size_t N>
-class sparse_array
-{
-  // Just a suggestion:
+class sparse_array {
+  typedef sparse_array<T, N> self_t;
+  typedef int                                    index_t;
+  typedef T                                      element_t;
 
-  typedef sparse_array<T, N>                      self_t;
-  typedef detail::sparse_array_proxy_ref<self_t>  proxy_reference;
-  typedef int                                     index_t;
+ public:
+  typedef index_t                                     difference_type;
 
-public:
-  using value_type 	           = T;
-  using size_type 	           = std::size_t;
-  using difference_type 	     = std::ptrdiff_t;
+  typedef T                                           value_type;
 
-  using reference 	           = detail::sparse_array_proxy_ref<self_t>;
-  using const_reference 	     = const T &; // Ha!
+  typedef detail::sparse_array_iterator<self_t>       iterator;
+  typedef detail::sparse_array_iterator<const self_t> const_iterator;
 
-  using pointer 	             = value_type*;
-  using const_pointer 	       = const value_type*;
-
-  using iterator               = detail::sparse_array_iterator<self_t>;
-  using const_iterator         = detail::sparse_array_iterator<const self_t>;
-  using reverse_iterator 	     = std::reverse_iterator<iterator>;
-  using const_reverse_iterator = std::reverse_iterator<const_iterator>;
+  typedef detail::sparse_array_proxy_ref<self_t>      reference;
+  typedef const T &                                   const_reference;
 
   friend iterator;
+  friend const_iterator;
   friend reference;
 
-public:
-  // Using = default unless there is a reason not to.
-  sparse_array()                           = default;
-  ~sparse_array()                          = default;
-
-  sparse_array(const self_t & other)       = default;
-  sparse_array(self_t && other)            = default;
-
-  self_t & operator=(const self_t & other) = default;
-  self_t & operator=(self_t && other)      = default;
-
-  bool operator==(const self_t & rhs) const {
-    // Just starting off with the usual stuff ...
-    return    (this == &rhs)                // identity
-           || (   _default == rhs._default  // equality, member-wise
-               && true // TODO
-              );
-    // Hm, actually looks really "default" until now.
-  }
-
-  bool operator!=(const self_t & rhs) const {
-    return not (*this == rhs);
-  }
-
-  constexpr size_type max_size() const noexcept {
-    return std::numeric_limits<int>::max();
-  }
-
-  constexpr size_type size() const noexcept {
-    // can use constexpr as N is known at compile time
-    return N;
-  }
-
-  constexpr bool empty() const noexcept {
-    return size() > 0;
-  }
-
-  iterator begin() noexcept {
-    return iterator(*this, 0);
-  }
-
-  const_iterator begin() const noexcept {
-    return const_iterator(*this, 0);
-  }
-
-  iterator end() noexcept {
-    return iterator(*this, size());
-  }
-
-  const_iterator end() const noexcept {
-    return const_iterator(*this, size());
-  }
-
-  reference front() {
-    return *begin();
-  }
-
-  const_reference front() const {
-    return *begin();
-  }
-
-  reference back() {
-    return *(begin() + size() - 1);
-  }
-
-  const_reference back() const {
-    return *(begin() + size() - 1);
-  }
-
-  // Write-access:
-  //
-  //   sa[34] /* proxy ref */ = 324;
-  //
-  reference operator[](int index) {
-    return reference(
-             // Now, what would this proxy reference depend on ...
-             // Well, we could make it depend on this sparse_array
-             // instance for starters:
-             *this,
-             // And the index, obviously:
-             index);
-  }
-
-  // Read-only-access
-  //
-  //   auto & val = sa[34];
-  //
-  const_reference operator[](int index) const {
-    return _value_or_default(index);
-  }
-
-  void fill(const value_type & value) {
-    // Assigning value to all values in container?
-    // HELL NO.
-    _values.clear();
-    // All elements will have the same value after this operation,
-    // so essentially we are changing the default.
-    //
-    // Dang, so witty.
-    //
-    _default = value;
-  }
-
-private:
-  const_reference _value_or_default(int index) const {
-    if (_values.find(index) != _values.end()) {
-      return _values[index];
+ public:
+  T _getValueOrDefault(const size_t pos) {
+    auto &it = _values.find(pos);
+    if (it == _values.end()) {
+      return _default_value;
     }
-    return _default;
+    return std::get<1>(*it);
   }
 
-  void _set_value(int _index, const T & value) {
-    _values[_index] = value;
+  const T &_cgetValueOrDefault(const size_t pos) const {
+    const auto &it = _values.find(pos);
+    if (it == _values.end()) {
+      return _default_value;
+    }
+    return std::get<1>(*it);
   }
 
-private:
-  T                               _default;
-  std::unordered_map<index_t, T>  _values;
+ private:
+  template <typename Compare = std::less<T>>
+  bool _compare(const self_t &other, Compare comp = Compare()) const {
+    if (_default_value != other._default_value) {
+      return comp(_default_value, other._default_value);
+    }
+    auto myit = _values.cbegin();
+    auto otit = other._values.cbegin();
+    while (myit != _values.end() && otit != other._values.end()) {
+      if (std::get<0>(*myit) == std::get<0>(*otit)) {
+        auto mval = std::get<1>(*myit);
+        auto oval = std::get<1>(*otit);
+        if (mval != oval) {
+          return comp(mval, oval);
+        }
+      } else {
+        if (std::get<0>(*myit) < std::get<0>(*otit)) {
+          return comp(std::get<1>(*myit), other._default_value);
+        } else {
+          return comp(_default_value, std::get<1>(*otit));
+        }
+      }
+      ++myit;
+      ++otit;
+    }
+    if (myit != _values.end()) {
+      return comp(std::get<1>(*myit), other._default_value);
+    } else if (otit != other._values.end()) {
+      return comp(_default_value, std::get<1>(*otit));
+    }
+    // empty containers
+    return comp(0, 0);
+  }
 
+ private:
+  std::map<size_t, T> _values;
+  T              _default_value = 0;
+  iterator       _begin         = iterator(*this, 0);
+  iterator       _end           = iterator(*this, N);
+  const_iterator _cbegin        = const_iterator(*this, 0);
+  const_iterator _cend          = const_iterator(*this, N);
+
+ public:
+  bool operator==(const self_t &other) const {
+    if (this == &other) {
+      return true;
+    }
+    if (_default_value != other._default_value) {
+      return false;
+    }
+    return _values == other._values;
+  }
+  bool operator!=(const self_t &other) const {
+    return !(*this == other);
+  }
+  bool operator<=(const self_t &other) const {
+    return _compare<std::less_equal<T>>(other);
+  }
+  bool operator>=(const self_t &other) const {
+    return _compare<std::greater_equal<T>>(other);
+  }
+  bool operator<(const self_t &other) const {
+    return _compare<std::less<T>>(other);
+  }
+  bool operator>(const self_t &other) const {
+    return _compare<std::greater<T>>(other);
+  }
+
+  const_reference at(const size_t pos) const {
+    if (!(pos < N)) {
+      throw std::out_of_range("index out of range");
+    }
+    return _cgetValueOrDefault(pos);
+  }
+
+  reference at(const size_t pos) {
+    if (!(pos < N)) {
+      throw std::out_of_range("index out of range");
+    }
+    return reference(*this, pos);
+  }
+
+  const_reference operator[](const size_t pos) const {
+    return _cgetValueOrDefault(pos);
+  }
+
+  reference operator[](const size_t pos) {
+    return reference(*this, pos);
+  }
+
+  reference       front() { return at(0); }
+  const_reference front() const { return at(0); }
+  reference       back() { return at(N - 1); }
+  const_reference back() const { return at(N - 1); }
+
+  // iterators
+  iterator        begin() { return _begin; }
+  const_iterator  begin() const { return const_iterator(*this, 0); }
+  const_iterator cbegin() const { return const_iterator(*this, 0); }
+  iterator       rbegin() { return --end(); }
+  const_iterator rbegin() const { return --end(); }
+
+  iterator          end() { return _end; }
+  const_iterator    end() const { return const_iterator(*this, N); }
+  const_iterator  &cend() const { return const_iterator(*this, N); }
+
+  // capacity
+  bool empty() const { return (N == 0); }
+
+  size_t size() const { return N; }
+
+  size_t max_size() const { return N; }
+
+  // operations
+  void fill(const T &value) {
+    _values.clear();
+    _default_value = value;
+  }
 };
 
-} // namespace cpppc
+}  // namespace cpppc
 
-#endif // CPPPC__S03__SPARSE_ARRAY_H__INCLUDED
+#endif  // CPPPC__S03__SPARSE_ARRAY_H__INCLUDED
