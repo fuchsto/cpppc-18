7f8f6c1df3da16c1cb2ad33c8cb5b5d099a229d0
Tobias Fuchs <twh.fuchs@gmail.com>
2018-06-07 20:43:43 +0000

    Range example

---
+++ session-08/01-range/range.h
@@ -0,0 +1,29 @@
+
+
+class Sentinel {
+  public:
+
+    Sentinel() = delete;
+    Sentinel(int n) : _num_compare(n) { }
+
+    template <class Iter>
+    bool operator!=(const Iter &) {
+      return --_num_compare > 0;
+    }
+
+  private:
+    int _num_compare;
+}
+
+
+
+
+auto sentinel = Sentinel(20);
+
+for (auto it = std::advance(std::begin(range), 40);
+     it != sentinel;
+     ++it)
+{
+
+}
+
