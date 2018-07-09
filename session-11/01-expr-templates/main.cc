acd995481da996ed882e5afa17ca0b7639a6fc60
Tobias Fuchs <fuchst@nm.ifi.lmu.de>
2018-07-09 11:51:12 +0200

    Adding session 11 code examples

---
+++ session-11/01-expr-templates/main.cc
@@ -0,0 +1,48 @@
+
+#include <iostream>
+
+// #include "vector_ops.h"
+#include "vector_et_ops.h"
+
+int main()
+{
+  // Now everybody, sing along!
+
+  //
+  // "With your state on the stack and your op well-defined ..."
+  //
+  cpppc::vector<int> v1 { 1, 2, 3, 4 };
+  cpppc::vector<int> v2 { 5, 6, 7, 8 };
+  cpppc::vector<int> v3 { 9, 0, 1, 2 };
+
+  //
+  // "You try this trick, and spin it, yeah ..."
+  //
+  std::cout << "instantiate vector_sum:" << std::endl;
+  // ,--- cpppc::vector_sum<int>
+  auto vsum = v1 + v2;
+
+  //
+  // "Your op will collapse, 'cause there's nothing in it, and
+  //  you ask yourself: where is my sum?"
+  //
+  std::cout << "assign vector_sum:" << std::endl;
+
+  //
+  // "There is my sum, there is my sum, theeeere is my sum ...
+  //
+  v3 = vsum;
+
+  //
+  //  log it out, on the stream, see it streamin' ...
+  //
+  for (auto e : v3) { std::cout << e << " "; }
+  std::cout << std::endl;
+
+  //
+  // "OOooooh-ooooooh ... oooooh-ooooh ..."
+  //
+
+  return 0;
+}
+
---
+++ session-11/01-expr-templates/vector_et_ops.h
@@ -0,0 +1,54 @@
+
+#include <vector>
+#include "macro.h"
+
+
+namespace cpppc {
+
+// Forward-declaration
+template <typename T> class vector;
+
+template <typename T>
+struct vector_sum {
+  vector_sum(const vector<T> & ix, const vector<T> & iy)
+  : x(ix), y(iy)
+  { }
+
+  T operator[](int i) const {
+    return x[i] + y[i];
+  }
+
+  // Q: What are the effects of const / reference members again?
+  //
+  // Q: Compare instantiation constraints of references to
+  //    instantiation
+  //    constraints of classes with const members.
+  //    Could it be that there is a conceptual relationship
+  //    between those?
+  const vector<T> & x, & y;
+};
+
+// Q: So, what is computed here?
+//
+template <typename T>
+vector_sum<T> operator+(
+  const vector<T> & x, const vector<T> & y) {
+  return { x, y }; // <- also see: uniform and generic initialization
+}
+
+
+template <typename T>
+class vector : public std::vector<T>
+{
+public:
+  vector<T>(std::initializer_list<T> lst) : std::vector<T>(lst)
+  { }
+
+  cpppc::vector<T> & operator=(const vector_sum<T> & rhs) {
+    for (std::size_t i = 0; i < this->size(); i++)
+      (*this)[i] = rhs[i];
+    return *this;
+  }
+};
+
+} // namespace cpppc
